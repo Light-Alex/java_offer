cbb5898fa6f5c2d6820db5dc05e3c26ab6d9c8bd
yan <245212467@qq.com>
2020-08-12 23:59:17 +0800

    最大矩形面积

---
+++ leetcode_9_1.java
@@ -0,0 +1,88 @@
+/**
+ * 题目：最大矩形
+ * 
+ * 题目描述：
+ * 给定一个仅包含 0 和 1 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+ * 
+ * 示例：
+ * 输入:
+    [
+        ["1","0","1","0","0"],
+        ["1","0","1","1","1"],
+        ["1","1","1","1","1"],
+        ["1","0","0","1","0"]
+    ]
+    输出: 6
+ */
+public class leetcode_9_1 {
+    public static void main(String[] args) {
+        leetcode_9_1 ll = new leetcode_9_1();
+        char[][] matrix = {
+            {'1','0','1','0','0'},
+            {'1','0','1','1','1'},
+            {'1','1','1','1','1'},
+            {'1','0','0','1','0'}
+        };
+        
+        ll.printRectangle(matrix);
+        System.out.println("=====================");
+        ll.maximalRectangle(matrix);
+    }
+
+    public void printRectangle(int[][] matrix){
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printRectangle(char[][] matrix){
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0) {
+            return 0;
+        }
+        //保存以当前数字结尾的连续 1 的个数
+        int[][] width = new int[matrix.length][matrix[0].length];
+        int maxArea = 0;
+        //遍历每一行
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[0].length; col++) {
+                //更新 width
+                if (matrix[row][col] == '1') {
+                    if (col == 0) {
+                        width[row][col] = 1;
+                    } else {
+                        width[row][col] = width[row][col - 1] + 1;
+                    }
+                } else {
+                    width[row][col] = 0;
+                }
+                //记录所有行中最小的数
+                int minWidth = width[row][col];
+                //向上扩展行
+                for (int up_row = row; up_row >= 0; up_row--) {
+                    int height = row - up_row + 1;
+                    //找最小的数作为矩阵的宽
+                    minWidth = Math.min(minWidth, width[up_row][col]);
+                    // System.out.println(minWidth);
+                    //更新面积
+                    maxArea = Math.max(maxArea, height * minWidth);
+                }
+            }
+        }
+
+        // printRectangle(width);
+        
+        return maxArea;
+    }
+}
